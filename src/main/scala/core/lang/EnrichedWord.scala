181e1f492c90deb82521e355fd9e005cd93cd96f
Kevin Hoffman <alothien@gmail.com>
2012-02-14 20:38:41 -0500

    Refactor enriched word for immutability

--- src/main/scala/core/lang/EnrichedWord.scala
+++ src/main/scala/core/lang/EnrichedWord.scala
@@ -8,20 +8,14 @@ case object Verb extends PartOfSpeech
 case object Adjective extends PartOfSpeech
 case object DontCare extends PartOfSpeech
 
-class EnrichedWord(value:String, tag:String) extends TaggedWord(value, tag) {	
-	private var _pos: PartOfSpeech = _
-	
-	def pos = _pos
-	def pos_= (value:PartOfSpeech):Unit = _pos = value 
-	
+class EnrichedWord(value:String, tag:String, val pos:PartOfSpeech) extends TaggedWord(value, tag) {		
 	
 	override def toString = "[EnrichedWord: word=" + value +", tag=" + tag + ", pos=" + pos + "]"
 }
 
 object EnrichedWord {
 	def apply(hw: TaggedWord) = {
-		val ew = new EnrichedWord(hw.value, hw.tag)		
-		ew.pos = rootTypeOf(ew.tag)
+		val ew = new EnrichedWord(hw.value, hw.tag, rootTypeOf(hw.tag))		
 		ew
 	}
 	
